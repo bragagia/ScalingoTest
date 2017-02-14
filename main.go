4c6dcc7d5b5ada3b587d7e4158ee2a7c33be9fc0
Mathias Bragagia <mathias.bragagia@epitech.eu>
2017-02-14 02:10:29 +0100

    Adds repos infos

--- main.go
+++ main.go
@@ -7,13 +7,30 @@ import (
 	"github.com/google/go-github/github"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	data := struct { }{ }
-	t, _ := template.ParseFiles("templates/index.html")
-	t.Execute(w, &data)
+type RepoInfos struct {
+	FullName string
+	Languages map[string]int
 }
 
-func GetList(q string) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, nil)
+}
+
+func GetRepoInfos(r github.Repository, ghc *github.Client, c chan RepoInfos) {
+	var ri RepoInfos
+	ri.FullName = *r.FullName
+
+	languages, _, err := ghc.Repositories.ListLanguages(*r.Owner.Login, *r.Name)
+	if err != nil {
+		//fmt.Print(err)
+	} else {
+		ri.Languages = languages
+	}
+	c <- ri
+}
+
+func GetList(q string) (*[100]RepoInfos) {
 	client := github.NewClient(nil)
 	opt := &github.SearchOptions{
 		ListOptions: github.ListOptions{PerPage: 100},
@@ -21,23 +38,36 @@ func GetList(q string) {
 	orgs, _, err := client.Search.Repositories("created:>=2017-02-13T23:25:00+01:00", opt)
 
 	if err != nil {
-		fmt.Print(err)
-	} else {
-		for i, _ := range orgs.Repositories {
-			fmt.Print("\n")
-			fmt.Print(i)
-		}
+		fmt.Print(err, "\n")
+		return nil
 	}
+
+	c := make(chan RepoInfos)
+
+	i := 0
+	for _, r := range orgs.Repositories {
+		go GetRepoInfos(r, client, c)
+		i++
+	}
+
+	var res [100]RepoInfos
+	for e := i; e > 0; e-- {
+		ri := <-c
+		res[e - 1] = ri
+	} 
+
+	return &res
 }
 
 func searchHandler(w http.ResponseWriter, r *http.Request) {
 	data := struct {
 		Query string
+		Repos [100]RepoInfos
 	}{
 		Query: r.URL.Query().Get("query"),
 	}
 
-	GetList(data.Query)
+	data.Repos = *GetList(data.Query)
 
 	t, err := template.ParseFiles("templates/search.html")
 	if err != nil {
